0b03cc1355f7c4f3b97e05268626accd6c61003b
creative dev <exam@gmail.com>
2014-03-21 12:28:11 +0800

    del

--- assets/js/ContentDBManager.js
+++
@@ -1,8 +0,0 @@
-function ContentDBManager() {
-	ContentDBManager.prototype.getBookData = function(dbc,cb) {
-		var query = "SELECT * from Pages";
-		dbc.transaction(query, function(res){
-			cb(res);
-		})		
-	};
-}
